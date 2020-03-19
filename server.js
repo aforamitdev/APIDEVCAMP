2ae0b6a1b5a802a28e335364bfb8eb8b9ab6bc44
amitrai <aforamitrai@gmail.com>
2020-03-20 00:57:54 +0530

    added morgan

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 const bootcamps = require("./routes/bootcamp");
+
+if (process.env.NODE_ENV === "undefined") {
+  app.use(morgan("dev"));
+}
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
