b3e413446ec38fc75d6f3c92c545eda78e22256c
amitrai <aforamitrai@gmail.com>
2020-03-22 20:50:56 +0530

    basic CURD functionalty for bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,39 +1,81 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc GET ALL bootcamps
 // @route GET api/v1/bootcamps
 // @access Public
 
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 // @desc GET single  bootcamps
 // @route GET api/v1/bootcamps/:id
 // @access Public
 
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "bootcamps byt id" });
+exports.getBootcamp = async (req, res, next) => {
+  console.log(req.params.id);
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 // @desc GET single  bootcamps
 // @route GET api/v1/bootcamps
 // @access Public
 
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "bootcamps byt id" });
-};
-
-// @desc DEL delete  bootcamps
-// @route DEL api/v1/bootcamps:id
-// @access private
-
-exports.deleteBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "bootcamps byt id" });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 // @desc PUT update  bootcamps
 // @route PUT api/v1/bootcamps:id
 // @access Privae
 
-exports.updateBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "bootcamps byt id" });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    return res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+// @desc DEL delete  bootcamps
+// @route DEL api/v1/bootcamps:id
+// @access private
+
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id, req.body);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    return res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+app.use(express.json());
 
 const bootcamps = require("./routes/bootcamp");
+connectDB();
 
 if (process.env.NODE_ENV === "undefined") {
   app.use(morgan("dev"));
@@ -13,7 +16,16 @@ if (process.env.NODE_ENV === "undefined") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
+  // @ts-ignore
   console.log(`SERVER RUNNING in ${process.env.NODE_ENV} made in ${PORT}`)
 );
+
+// handle unhandle prommis rejection
+process.on("unhandledRejection", (err, prommis) => {
+  console.log(`ERROR: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
