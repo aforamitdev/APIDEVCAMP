1784d2c12e04b638ffb91c9805d0d38b43571335
amitrai <aforamitrai@gmail.com>
2020-03-23 11:55:09 +0530

    erroe handler added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,18 +1,15 @@
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/asyncHandler");
 const Bootcamp = require("../models/Bootcamp");
 
 // @desc GET ALL bootcamps
 // @route GET api/v1/bootcamps
 // @access Public
 
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
-    res.status(200).json({ success: true, data: bootcamps });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  res.status(200).json({ success: true, data: bootcamps });
+});
 
 // @desc GET single  bootcamps
 // @route GET api/v1/bootcamps/:id
